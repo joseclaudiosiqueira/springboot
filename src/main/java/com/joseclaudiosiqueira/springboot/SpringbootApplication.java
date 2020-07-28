aca6a739853f223654b9440c8d75d1ae629be97d
Jose Claudio [ZC] <joseclaudio.siqueira@gmail.com>
2020-07-28 19:25:30 -0300

    Instantiating and Inserting data automatically

--- src/main/java/com/joseclaudiosiqueira/springboot/SpringbootApplication.java
+++ src/main/java/com/joseclaudiosiqueira/springboot/SpringbootApplication.java
@@ -1,13 +1,33 @@
 package com.joseclaudiosiqueira.springboot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringbootApplication {
+import com.joseclaudiosiqueira.springboot.domain.Category;
+import com.joseclaudiosiqueira.springboot.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class SpringbootApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Category category1 = new Category(null, "Informática");
+		Category category2 = new Category(null, "Escritório");
+		
+		categoryRepository.saveAll(Arrays.asList(category1, category2));
+
 	}
 
 }
