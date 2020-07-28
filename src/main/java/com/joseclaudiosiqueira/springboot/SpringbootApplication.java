8787355d7209e2c2963499cbf830e7167a7eaa22
Jose Claudio [ZC] <joseclaudio.siqueira@gmail.com>
2020-07-28 19:57:54 -0300

    Association between Category and Products ManyToMany

--- src/main/java/com/joseclaudiosiqueira/springboot/SpringbootApplication.java
+++ src/main/java/com/joseclaudiosiqueira/springboot/SpringbootApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.joseclaudiosiqueira.springboot.domain.Category;
+import com.joseclaudiosiqueira.springboot.domain.Product;
 import com.joseclaudiosiqueira.springboot.repositories.CategoryRepository;
+import com.joseclaudiosiqueira.springboot.repositories.ProductRepository;
 
 @SpringBootApplication
 public class SpringbootApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+	@Autowired
+	private ProductRepository productRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
@@ -26,7 +30,20 @@ public class SpringbootApplication implements CommandLineRunner {
 		Category category1 = new Category(null, "Informática");
 		Category category2 = new Category(null, "Escritório");
 		
+		Product product1 = new Product (null, "Computador", 2000.00);
+		Product product2 = new Product (null, "Impressora", 800.00);
+		Product product3 = new Product (null, "Mouse", 80.00);
+		
+		category1.getProducts().addAll(Arrays.asList(product1, product2, product3));
+		category2.getProducts().addAll(Arrays.asList(product2));
+		
+		product1.getCategories().addAll(Arrays.asList(category1));
+		product2.getCategories().addAll(Arrays.asList(category1, category2));
+		product3.getCategories().addAll(Arrays.asList(category1));
+		
 		categoryRepository.saveAll(Arrays.asList(category1, category2));
+		productRepository.saveAll(Arrays.asList(product1,product2,product3));
+	
 
 	}
 
