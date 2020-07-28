a99a9dd9f726d59d2ea4fcc106d04137af02e631
Jose Claudio [ZC] <joseclaudio.siqueira@gmail.com>
2020-07-28 17:26:12 -0300

    Testing first domain class

--- src/main/java/com/joseclaudiosiqueira/springboot/resources/CategoryResource.java
+++ src/main/java/com/joseclaudiosiqueira/springboot/resources/CategoryResource.java
@@ -1,14 +1,27 @@
 package com.joseclaudiosiqueira.springboot.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.joseclaudiosiqueira.springboot.domain.Category;
+
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
+	
 	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return "The REST is already!";
+	public List<Category> list() {
+		Category cat1 = new Category(1, "Informatica");
+		Category cat2 = new Category(2, "Escritório");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 }
