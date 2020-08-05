eb26cd78fd4017d3825889b562c1fef0f30e9a9a
Jose Claudio [ZC] <joseclaudio.siqueira@gmail.com>
2020-08-05 17:57:29 -0300

    Adding a @Transactional annotation

--- src/main/java/com/joseclaudiosiqueira/springboot/services/ClientService.java
+++ src/main/java/com/joseclaudiosiqueira/springboot/services/ClientService.java
@@ -9,9 +9,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.joseclaudiosiqueira.springboot.domain.Client;
 import com.joseclaudiosiqueira.springboot.dto.DTOClient;
+import com.joseclaudiosiqueira.springboot.repositories.AddressRepository;
 import com.joseclaudiosiqueira.springboot.repositories.ClientRepository;
 import com.joseclaudiosiqueira.springboot.services.exceptions.DataIntegrityException;
 import com.joseclaudiosiqueira.springboot.services.exceptions.ObjectNotFoundException;
@@ -20,43 +22,53 @@ import com.joseclaudiosiqueira.springboot.services.exceptions.ObjectNotFoundExce
 public class ClientService {
 
 	@Autowired
-	private ClientRepository repository;
+	private ClientRepository clientRepository;
+	@Autowired
+	private AddressRepository addressRepository;
 
 	public Client find(Integer id) throws ObjectNotFoundException {
-		Optional<Client> object = repository.findById(id);
+		Optional<Client> object = clientRepository.findById(id);
 		return object.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Client.class.getName()));
 
+	}
+	
+	@Transactional
+	public Client insert(Client client) {
+		client.setId(null);
+		client = clientRepository.save(client);
+		addressRepository.saveAll(client.getAddresses());
+		return client;
 	}
 
 	public Client update(Client client) {
 		Client updatingClient = find(client.getId());
 		updateClient(updatingClient, client);
-		return repository.save(updatingClient);
+		return clientRepository.save(updatingClient);
 	}
 
 	public void delete(Integer id) {
 		find(id);
 		try {
-			repository.deleteById(id);
+			clientRepository.deleteById(id);
 		} catch (DataIntegrityViolationException exception) {
 			throw new DataIntegrityException("You can't delete Clients that have data associated (e.g. orders).");
 		}
 	}
 
 	public List<Client> findAll() {
-		return repository.findAll();
+		return clientRepository.findAll();
 	}
 
 	public Page<Client> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		return repository.findAll(pageRequest);
+		return clientRepository.findAll(pageRequest);
 	}
 
 	public Client fromDTO(DTOClient dtoClient) {
 		return new Client(dtoClient.getId(), dtoClient.getName(), dtoClient.getEmail(), null, null);
 	}
-
+	
 	private void updateClient(Client updatingClient, Client client) {
 		updatingClient.setName(client.getName());
 		updatingClient.setEmail(client.getEmail());
